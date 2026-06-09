b9323269991333153272354a1b3b121d4abe23ad
Pablo <pablosociales2020@hotmail.com>
2026-06-08 21:57:38 -0600

    Update

---
+++ DataAccess/CRUD/MovieCrudFactorycs.cs
@@ -0,0 +1,73 @@
+﻿using Balanceless.DAO;  // ← Cambia DataAccess.DAO por Balanceless.DAO
+using Entities_DTOs;
+using Entities_DTOs.Entities_DTOs;
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess.CRUD
+{
+    public class CrudMovieFactory : CrudFactory
+    {
+        public CrudMovieFactory()
+        {
+            sqlDao = SqlDao.GetInstance();  // Ya no necesitas DataAccess.DAO.
+        }
+
+        public override void Create(BaseDTO baseDTO)
+        {
+            var movie = baseDTO as Movie;
+
+            var sqlOperation = new SqlOperation();  // Ya no necesitas DataAccess.DAO.
+            sqlOperation.ProcedureName = "CRE_MOVIE_PR";
+
+            sqlOperation.AddStringParameter("P_TITLE", movie.Title);
+            sqlOperation.AddStringParameter("P_SINOPSIS", movie.Sinopsis);
+            sqlOperation.AddStringParameter("P_GENRE", movie.Genre);
+            sqlOperation.AddIntParameter("P_DURATION", movie.Duration);
+            sqlOperation.AddStringParameter("P_CLASIFICATION", movie.Clasification);
+            sqlOperation.AddStringParameter("P_IMAGE", movie.Image);
+            sqlOperation.AddStringParameter("P_STATUS", movie.Status);
+
+            sqlDao.ExecuteProcedure(sqlOperation);
+        }
+
+        public override void Update(BaseDTO baseDTO)
+        {
+            var movie = baseDTO as Movie;
+
+            var sqlOperation = new SqlOperation();
+            sqlOperation.ProcedureName = "UPD_MOVIE_PR";
+
+            sqlOperation.AddIntParameter("P_ID", movie.Id);
+            sqlOperation.AddStringParameter("P_TITLE", movie.Title);
+            sqlOperation.AddStringParameter("P_SINOPSIS", movie.Sinopsis);
+            sqlOperation.AddStringParameter("P_GENRE", movie.Genre);
+            sqlOperation.AddIntParameter("P_DURATION", movie.Duration);
+            sqlOperation.AddStringParameter("P_CLASIFICATION", movie.Clasification);
+            sqlOperation.AddStringParameter("P_IMAGE", movie.Image);
+
+            sqlDao.ExecuteProcedure(sqlOperation);
+        }
+
+        public override void Delete(BaseDTO baseDTO)
+        {
+            var movie = baseDTO as Movie;
+
+            var sqlOperation = new SqlOperation();
+            sqlOperation.ProcedureName = "DEL_MOVIE_PR";
+            sqlOperation.AddIntParameter("P_ID", movie.Id);
+
+            sqlDao.ExecuteProcedure(sqlOperation);
+        }
+
+        public override T RetrieveById<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/CRUD/TicketCrudFactory.cs
@@ -0,0 +1,66 @@
+﻿using Balanceless.DAO;
+using Entities_DTOs;
+using Entities_DTOs.Entities_DTOs;
+
+namespace DataAccess.CRUD
+{
+    public class TicketCrudFactory : CrudFactory
+    {
+        public TicketCrudFactory()
+        {
+            sqlDao = SqlDao.GetInstance();
+        }
+
+        public override void Create(BaseDTO baseDTO)
+        {
+            var ticket = baseDTO as Ticket;
+
+            var sqlOperation = new SqlOperation();
+            sqlOperation.ProcedureName = "CRE_TICKET_PR";
+
+            sqlOperation.AddDateTimeParameter("P_DATE", ticket.Date);
+            sqlOperation.AddStringParameter("P_SCHEDULE", ticket.Schedule);
+            sqlOperation.AddStringParameter("P_PRICE", ticket.Price.ToString());
+            sqlOperation.AddStringParameter("P_TYPE", ticket.Type);
+
+            sqlDao.ExecuteProcedure(sqlOperation);
+        }
+
+        public override void Update(BaseDTO baseDTO)
+        {
+            var ticket = baseDTO as Ticket;
+
+            var sqlOperation = new SqlOperation();
+            sqlOperation.ProcedureName = "UPD_TICKET_PR";
+
+            sqlOperation.AddIntParameter("P_ID", ticket.Id);
+            sqlOperation.AddDateTimeParameter("P_DATE", ticket.Date);
+            sqlOperation.AddStringParameter("P_SCHEDULE", ticket.Schedule);
+            sqlOperation.AddStringParameter("P_PRICE", ticket.Price.ToString());
+            sqlOperation.AddStringParameter("P_TYPE", ticket.Type);
+
+            sqlDao.ExecuteProcedure(sqlOperation);
+        }
+
+        public override void Delete(BaseDTO baseDTO)
+        {
+            var ticket = baseDTO as Ticket;
+
+            var sqlOperation = new SqlOperation();
+            sqlOperation.ProcedureName = "DEL_TICKET_PR";
+            sqlOperation.AddIntParameter("P_ID", ticket.Id);
+
+            sqlDao.ExecuteProcedure(sqlOperation);
+        }
+
+        public override T RetrieveById<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
